ae591f40fb70e6cbe18927c47a0e21513a7eef5a
h0adaynd <hoadd1@hblab.vn>
2020-04-23 20:45:03 +0700

    Make auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'namespace' => 'Admin',
+    'prefix' => 'admin',
+], function () {
+    Route::group([
+        'namespace' => 'Auth'
+    ], function () {
+        Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
+    });
+});
