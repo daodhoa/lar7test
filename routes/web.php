6c1f929ae51ab589c5687e8db7a676c469588608
daodhoa <duyhoa.dao1207@gmail.com>
2020-09-27 22:08:35 +0700

    Fake

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.profile.index');
-});
+})->name('admin.profile.index');
 
 Route::group([
     'namespace' => 'Admin',
